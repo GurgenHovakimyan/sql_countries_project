943b7a371e059b17a895b2ee3349aa7f822df5ac
Gurgen <hovakimyan.gurgen.1997@gmail.com>
2021-01-26 20:32:26 +0400

    26JAN2021
    Final changes

--- scripts/Joing_data_and_creating_summary.sql
+++ scripts/Joing_data_and_creating_summary.sql
@@ -43,6 +43,7 @@ from languages l
 group by code
 order by number_of_languages desc;
 
+-- for our region
 select code as country, count(*) as number_of_languages
 from languages l 
 where l.code = 'ARM'
@@ -80,13 +81,11 @@ group by region)
 and populations.year = 2015
 order by population_density desc;
 
-
--- partition by region
+-- partition by region for calculating min, average and maximum surface_area for each region
 select distinct(region),  MIN(surface_area) over (partition by region) as minimum_surface_area,
 AVG(surface_area) over (partition by region) as average_surface_area,
 MAX(surface_area) over (partition by region) as maximum_surface_area
 from countries;
-
 
 -- subquery in from statement for counting usage of currencies
 select countries.name as country, sub.currency as currency_count
@@ -98,5 +97,46 @@ from countries,
   WHERE countries.code = sub.code
 ORDER BY currency_count desc;
 
+-- calculating the amount of savings in each currency.
+select  basic_unit as Currency, sum(gross_savings) as Savings
+from economies e
+right join currencies c on c.code = e.code
+where year = 2015
+group by basic_unit 
+order by Savings DESC NULLS LAST;
 
+-- calculating the average unemployment rate and GDP per capita for countries with differnet governing forms.
+select distinct c.gov_form, avg(e.unemployment_rate) over (partition by c.gov_form) as average_unemployment,
+avg(gdp_percapita)over (partition by c.gov_form) as avergae_GDP_per_capita
+from countries c right join economies e on c.code = e.code
+where e.year = 2015 and c.gov_form is not null
+order by average_unemployment desc nulls last;
 
+--- creating a table with top 2-12 countries with the highest amount of investment in the year of 2015.
+select c.code, c.name, c.continent, e.total_investment
+from countries c left join economies e on c.code = e.code
+where e.year = 2015 and c.code in 
+	(select e.code
+	from economies e
+	where e.total_investment>40 AND e.year = 2015)
+limit 10 offset 2;
+
+-- calculating the average unemployment rate and GDP per capita for countries with differnet froms of governing.
+select c.region || '/ ' || c.name as country_region,  a.imports 
+from (select code, imports
+	 from economies
+	 where imports > 35 ) a
+inner join countries c 
+on c.code = a.code
+order by imports asc nulls last 
+limit 10;
+
+-- counting the number of countries per combination of geographical size group and income group.
+select geosize_group,income_group, count(*) as number_of_countries_per_group
+from countries c
+left join countries_plus cp 
+using (code) 
+left join economies e 
+using (code) 
+group by geosize_group, income_group
+order by geosize_group desc;
